d61310637878d18dab5bcc6d2df740c5aee2683c
Maarten Weyns <maarten.weyns@mawey.be>
2023-03-23 10:08:52 +0100

    Update get_size.sh to account for truncation

--- bash/get_size.sh
+++ bash/get_size.sh
@@ -3,18 +3,25 @@ NAME_LIST=$(tail -n +2 domains_and_nameservers.csv | awk -F, '{print $2}')
 
 DOMAINS=($DOMAINS)
 COUNT=0
+echo "domain,name,request,response,truncated"
 for NAMES in $NAME_LIST
 do
     DOMAIN=${DOMAINS[COUNT]}
     COUNT=$((COUNT+1))
     NAMES=$(echo $NAMES | tr ";" " ")
-    # Request size is 60 + length domain name
     for NAME in $NAMES
     do
-        REQUEST=$(echo ${#NAME})
-        REQUEST=$((REQUEST+60))
-        RESPONSE=$(host -U -v -t any -4 $DOMAIN $NAME | tail -n 1 | awk '{print $2}')
-        echo "$DOMAIN,$NAME,$REQUEST,$RESPONSE"
+        REQUEST=$(echo ${#DOMAIN})
+        REQUEST=$((REQUEST+83))
+        OUTPUT=$(dig -4 +notcp +ignore +bufsize=4096 @$NAME $DOMAIN ANY)
+        TC_FLAG=$(echo "$OUTPUT" | grep -o "\btc\b" )
+        RESPONSE=$(echo "$OUTPUT" | tail -n 2 | grep -E -o "\brcvd: [0-9]+\b" | awk '{print $2}')
+        if [[ "$TC_FLAG" == "tc" ]]
+        then
+            echo "$DOMAIN,$NAME,$REQUEST,$RESPONSE,yes"
+        else
+            echo "$DOMAIN,$NAME,$REQUEST,$RESPONSE,no"
+        fi
+        sleep 1
     done
 done
-
