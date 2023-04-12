489bbd32062afb18381c489c13e89b9dc684bd7e
Gregor Schram <gschram@protonmail.ch>
2023-04-12 23:05:38 +0200

    Pretty plots

--- python/plot_amplification.py
+++ python/plot_amplification.py
@@ -1,92 +1,86 @@
 import sys
-import pandas as pd
+
 import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd
 
 
-def create_barcharts(csv_path, output_top10, output_bottom10):
-    df = pd.read_csv(csv_path, sep=",")
+def main(csv_file, domain_output, ns_output):
+    # Read the CSV file
+    df = pd.read_csv(csv_file)
 
-    # Calculate the amplification factor
+    # Drop rows with missing values in 'request' or 'response' columns
+    df = df.dropna(subset=['request', 'response'])
+
+    # Calculate amplification
     df['amplification'] = df['response'] / df['request']
 
-    # Group by domain and name, and compute the average amplification factor
-    domain_avg = df.groupby(['domain', 'truncated']).mean().reset_index()
-    name_avg = df.groupby(['name', 'truncated']).mean().reset_index()
+    # Separate truncated and non-truncated data
+    df_truncated = df[df['tc'] == 'tc']
+    df_not_truncated = df[df['tc'] != 'tc']
 
-    # Create top 10 and bottom 10 domain and name DataFrames with truncated information
-    domain_agg = domain_avg.groupby(
-        'domain')['amplification'].mean().reset_index()
-    domain_agg = domain_avg.merge(
-        domain_agg, on='domain', suffixes=('', '_mean'))
+    # Calculate amplification per domain and nameserver
+    domain_amplification_truncated = df_truncated.groupby('domain')[
+        'amplification'].mean()
+    domain_amplification_not_truncated = df_not_truncated.groupby('domain')[
+        'amplification'].mean()
 
-    name_agg = name_avg.groupby('name')['amplification'].mean().reset_index()
-    name_agg = name_avg.merge(name_agg, on='name', suffixes=('', '_mean'))
+    ns_amplification_truncated = df_truncated.groupby(
+        'nameserver')['amplification'].mean()
+    ns_amplification_not_truncated = df_not_truncated.groupby('nameserver')[
+        'amplification'].mean()
 
-    domain_top10 = domain_agg.nlargest(10, 'amplification_mean')
-    domain_bottom10 = domain_agg.nsmallest(10, 'amplification_mean')
-    name_top10 = name_agg.nlargest(10, 'amplification_mean')
-    name_bottom10 = name_agg.nsmallest(10, 'amplification_mean')
+    # Plot top and bottom 10 domains
+    domain_amplification_sorted = domain_amplification_not_truncated.add(
+        domain_amplification_truncated, fill_value=0).sort_values(ascending=False)
+    top_domains = domain_amplification_sorted.head(10).index
+    bottom_domains = domain_amplification_sorted.tail(10).index
 
-    def plot_barchart(df, ylabel, title, output_path):
-        fig, ax = plt.subplots()
+    top_bottom_domains_truncated = domain_amplification_truncated.reindex(top_domains).fillna(
+        0).append(domain_amplification_truncated.reindex(bottom_domains).fillna(0))
+    top_bottom_domains_not_truncated = domain_amplification_not_truncated.reindex(top_domains).fillna(
+        0).append(domain_amplification_not_truncated.reindex(bottom_domains).fillna(0))
 
-        # Set width of bars
-        bar_width = 0.25
+    plt.figure(figsize=(10, 6))
+    top_bottom_domains_truncated.plot(
+        kind='bar', color='#377eb8', label='Truncated')
+    top_bottom_domains_not_truncated.plot(
+        kind='bar', bottom=top_bottom_domains_truncated, color='#ff7f00', label='Not Truncated')
+    plt.xlabel('Domain')
+    plt.ylabel('Amplification')
+    plt.title('Top and Bottom 10 Domains by Amplification')
+    plt.legend()
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.5)
+    plt.savefig(domain_output)
+    plt.clf()
 
-        # Set positions of bars on the X-axis
-        ind = np.arange(len(df[ylabel].unique()))
-        bar_positions = [ind, ind + bar_width, ind + 2 * bar_width]
+    # Plot top and bottom 10 nameservers
+    ns_amplification_sorted = ns_amplification_not_truncated.add(
+        ns_amplification_truncated, fill_value=0).sort_values(ascending=False)
+    top_ns = ns_amplification_sorted.head(10).index
+    bottom_ns = ns_amplification_sorted.tail(10).index
 
-        # Create bars for different truncated categories
-        for i, trunc in enumerate(['yes', 'no', None]):
-            if trunc is None:
-                trunc_df = df
-            else:
-                trunc_df = df[df['truncated'] == trunc]
-            mean_values = trunc_df.groupby(ylabel)['amplification'].mean()
-            label = 'Truncated Ignore' if trunc is None else f'Truncated {trunc.capitalize()}'
+    top_bottom_ns_truncated = ns_amplification_truncated.reindex(top_ns).fillna(
+        0).append(ns_amplification_truncated.reindex(bottom_ns).fillna(0))
+    top_bottom_ns_not_truncated = ns_amplification_not_truncated.reindex(top_ns).fillna(
+        0).append(ns_amplification_not_truncated.reindex(bottom_ns).fillna(0))
 
-            # Align the data to have the same index as the X-axis ticks
-            aligned_values = [mean_values.get(
-                entry, 0) for entry in df[ylabel].unique()]
-
-            ax.bar(bar_positions[i], aligned_values,
-                   width=bar_width, label=label)
-
-        # Set the labels, title, and legend
-        ax.set_xticks(ind + bar_width)
-        ax.set_xticklabels(df[ylabel].unique())
-        ax.set_ylabel('Average Amplification')
-        ax.set_title(title)
-        ax.legend()
-
-        # Save the plot
-        plt.xticks(rotation=90)
-        plt.savefig(output_path)
-        plt.close()
-
-    def plot_combined_barchart(df_top10, df_bottom10, ylabel, title, output_path):
-        # Combine top 10 and bottom 10 DataFrames
-        df_combined = pd.concat([df_top10, df_bottom10]).reset_index(drop=True)
-
-        # Call the plot_barchart function
-        plot_barchart(df_combined, ylabel, title, output_path)
-
-    # Create the combined bar charts
-    plot_combined_barchart(domain_top10, domain_bottom10, 'domain',
-                           'Top and Bottom 10 Domains', output_domains_plot)
-    plot_combined_barchart(name_top10, name_bottom10,
-                           'name', 'Top and Bottom 10 Names', output_dns_plot)
+    plt.figure(figsize=(10, 6))
+    top_bottom_ns_truncated.plot(kind='bar', color='#377eb8', label='Truncated')
+    top_bottom_ns_not_truncated.plot(
+        kind='bar', bottom=top_bottom_ns_truncated, color='#ff7f00', label='Not Truncated')
+    plt.xlabel('Nameserver')
+    plt.ylabel('Amplification')
+    plt.title('Top and Bottom 10 Nameservers by Amplification')
+    plt.legend()
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.5)
+    plt.savefig(ns_output)
 
 
-if len(sys.argv) < 4:
-    print("Usage: python plot_amplification.py <csv_file> <output_domains_plot> <output_dns_plot>")
-    sys.exit()
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print('Usage: python script.py <csv_file> <domain_output> <ns_output>')
+        sys.exit(1)
 
-# Read the CSV file
-csv_file = sys.argv[1]
-output_domains_plot = sys.argv[2]
-output_dns_plot = sys.argv[3]
-
-create_barcharts(csv_file, output_domains_plot, output_dns_plot)
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
